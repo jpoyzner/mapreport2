52a951bf91038c22bbbe6c696ff849a24c73a512
Jeff Poyzner <holidaydoctor@hotmail.com>
2014-08-02 21:57:16 -0700

    jeff's local changes

--- js/console.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    var fileref = document.createElement('script');
-    fileref.setAttribute('type', 'text/javascript');
-    fileref.setAttribute('src', '//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.10/require.min.js');
-    fileref.setAttribute('data-main', '//50.62.80.222/MapReport/js/consolemodule.js'); //or wherever it's hosted currently
-    document.getElementsByTagName('head')[0].appendChild(fileref);
-})();
--- js/templates/optionshtml.js
+++
@@ -1,7 +0,0 @@
-define([], function() { return '' +
-	'<div id="mr-options">' +
-		'<div class="mr-option">TOPIC</div>' +
-		'<div class="mr-option">LOCATION</div>' +
-		'<div class="mr-option">TIME</div>' +
-	'</div>';
-});
