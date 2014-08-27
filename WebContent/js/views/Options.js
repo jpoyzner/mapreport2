38ebd9e831197776520e441ed0ff14a8bcf638c9
Jeff Poyzner <holidaydoctor@hotmail.com>
2014-08-27 02:48:12 -0700

    added input options web components and related code

--- WebContent/js/views/Options.js
+++ WebContent/js/views/Options.js
@@ -4,6 +4,17 @@ define(['templates', 'utils/css', 'backbone', 'underscore'], function(Templates,
 		initialize: function(options) {
 			Css.load('options');
 			this.$el.html(Templates['mr-options-template'](options.news));
+			
+			$('.mr-option').click(function(e) {
+				if ($(e.target).is('input-options')) {
+					return;
+				}
+				
+				//this is bullshit but works, can't be done with trigger
+				var e1 = document.createEvent("MouseEvents");
+				e1.initMouseEvent("mousedown", true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+				$(this).find('input-options')[0].dispatchEvent(e1);	
+			});
 		}
 	});
 });
