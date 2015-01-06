ebc7184aa2f8c3afbf647d39bd6e89534a15855f
Jeff Poyzner <holidaydoctor@hotmail.com>
2015-01-06 00:53:05 -0800

    added colors to map pins

--- WebContent/js/models/story.js
+++ WebContent/js/models/story.js
@@ -1,11 +1,13 @@
-define(['underscore', 'backbone'], function() {
+define(['utils/color', 'underscore', 'backbone'], function(Color) {
 	return Backbone.Model.extend({
 		initialize: function(data) {
 			//TODO: replace with a pre-generated topic icon and number or a way to combine the two:
 			
 			//DOC here:https://developers.google.com/chart/image/docs/gallery/dynamic_icons
 			//THIS IS TECHINCALY ALREADY DEPRECTAED!
-			this.set('icon', 'http://chart.apis.google.com/chart?chst=d_map_spin&chld=.75|0|FF0000|12|_|' + data.mapLabel);
+			this.set(
+				'icon',
+				'http://chart.apis.google.com/chart?chst=d_map_spin&chld=.75|0|' + Color.random().slice(1) + '|12|_|' + data.mapLabel);
 			//OLD DEFAULT ICON: 'http://www.mapreport.com/images/common/list5.gif'
 			
 			/*RELATED LINKS:
