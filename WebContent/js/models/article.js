c2db16648ddc6c9416b15e7502c9e7e49b5bd122
Jeff Poyzner <holidaydoctor@hotmail.com>
2015-01-11 18:12:15 -0800

    some refactoring

---
+++ WebContent/js/models/article.js
@@ -0,0 +1,22 @@
+define(['utils/color', 'underscore', 'backbone'], function(Color) {
+	return Backbone.Model.extend({
+		initialize: function(data) {
+			//TODO: replace with a pre-generated topic icon and number or a way to combine the two:
+			
+			//DOC here:https://developers.google.com/chart/image/docs/gallery/dynamic_icons
+			//THIS IS TECHINCALY ALREADY DEPRECTAED!
+			this.set(
+				'icon',
+				'http://chart.apis.google.com/chart?chst=d_map_spin&chld=.75|0|' + Color.random().slice(1) + '|12|_|' + data.mapLabel);
+			//OLD DEFAULT ICON: 'http://www.mapreport.com/images/common/list5.gif'
+			
+			/*RELATED LINKS:
+			 * http://stackoverflow.com/questions/3562506/add-numbering-label-to-google-map-marker?lq=1
+			 * http://stackoverflow.com/questions/2436484/how-can-i-create-numbered-map-markers-in-google-maps-v3
+			 * http://stackoverflow.com/questions/16734830/google-maps-custom-marker-with-number
+			 * http://stackoverflow.com/questions/2890670/google-maps-place-number-in-marker
+			 * https://developers.google.com/maps/documentation/javascript/markers
+			 */
+		}
+	});
+});
