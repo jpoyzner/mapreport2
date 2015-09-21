bb6c2ef86f4c8ca0fa7aa1891573cb0174c6f09f
Jeff Poyzner <holidaydoctor@hotmail.com>
2015-09-20 21:58:57 -0700

    report component ready in react

--- js/components/page.js
+++ js/components/page.js
@@ -4,18 +4,28 @@ function(React, Detector, News, Options, Map, Report, Router) {
 		getInitialState: function() {
 			window.page = this;
 			
-			new News("http://" + Router.rootDomain + Router.pathPrefix, Router.settings).on('sync', function(news) {
-		    	this.setState({news: news, latitude: 37.759753, longitude: -122.50232699999998}); //won't need coordinates probably
-		    }.bind(this));
+			new News("http://" + Router.rootDomain + Router.pathPrefix, Router.settings)
+				.on('sync', function(news) {
+					//won't need coordinates probably
+			    	this.setState({loading: false, news: news, latitude: 37.759753, longitude: -122.50232699999998});
+			    }.bind(this))
+			    .on('request', function() {
+			    	this.setState({loading: true});
+			    }.bind(this));
 			
-		    return {};
+		    return {loading: true};
 		},
 		render: function() {
 			return (
 				<div id="mr-buckets" className={Detector.phone() ? 'mobile' : 'desktop'}>
 					<Options news={this.state.news} />
 					<Map news={this.state.news} latitude={this.state.latitude} longitude={this.state.longitude} />
-					<Report news={this.state.news} />
+					<div id="mr-report-bucket">
+						{(this.state.loading || this.state.news.fetches) ?
+							"LOADER"
+							: <Report news={this.state.news} />
+						}
+					</div>
 				</div>
 			);
 		}
--- js/components/report.js
+++ js/components/report.js
@@ -1,8 +1,21 @@
-define(['react'], function(React, Detector, Options, Map, Report, Router) {
+define(['react', 'utils/css', 'utils/color'], function(React, Css, Color) {
 	return React.createClass({
+		getInitialState: function() {
+			Css.load('report');		
+			return {};
+		},
 		render: function() {
 			return (
-				<div id="mr-report-bucket">WHITE LOADER</div>	
+				<div id="mr-report">
+					{this.props.news.models.map(function(article) {
+						return (
+							<div className="mr-report-article" style={{"background-color": article.get('colorHex')}}>
+								<img src={article.get('icon')}></img>
+								<a href={article.get('url')} target="_blank"><p />{article.get('mapLabel')}: {article.get('dateTime')}: {article.get('label')}<p /></a>
+							</div>
+						);
+					})}
+				</div>		
 			);
 		}
 	});
